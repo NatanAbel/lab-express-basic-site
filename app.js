e05d0eef4f9ab0861bc1ff797070c2a26c205180
Abiel Isak <abizmo99@gmail.com>
2023-01-31 20:10:36 +0100

    Create path between static pages

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const app = express();
 const PORT = 3000
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    try{
+        res.sendFile(__dirname + '/views/home.html')
+    }catch(err){
+        console.log(err);
+    }
+})
+app.get('/about', (req, res) => {
+    try{
+        res.sendFile(__dirname + '/views/about.html')
+    }catch(err){
+        console.log(err);
+    }
+})
+app.get('/works', (req, res) => {
+    try{
+        res.sendFile(__dirname + '/views/works.html')
+    }catch(err){
+        console.log(err);
+    }
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port `);
